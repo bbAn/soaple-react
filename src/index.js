6f9735ed263db06fb8bde9f2f5aabe4b3d1d3cfa
2220140 <2220140@ad.h-firework.com>
2022-06-22 10:02:21 +0900

    [시계만들기] 렌더링된 엘리먼트 업데이트하기

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from './ch_03/Library';
+import Clock from './ch_04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
+setInterval (() => {
   root.render(
     <React.StrictMode>
-      <Library />
+      <Clock />
     </React.StrictMode>
   );
+  }, 1400);
 
 
 // If you want to start measuring performance in your app, pass a function
